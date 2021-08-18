ea8b4df55c724fa53fea13530dcd440ac3f30f2b
André Pereira <andre.pereira.fisica@gmail.com>
2021-08-17 23:20:57 -0300

    implementação de verificação de usuario

--- config/routes.php
+++ config/routes.php
@@ -5,8 +5,10 @@ require __DIR__ . '/../vendor/autoload.php';
 use Alura\Cursos\Controller\Exclusao;
 use Alura\Cursos\Controller\FormularioEdicao;
 use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\FormularioLogin;
 use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\Persistencia;
+use Alura\Cursos\Controller\RealizarLogin;
 
 $rotas = [
     '/listar-cursos'    => ListarCursos::class,
@@ -14,6 +16,8 @@ $rotas = [
     '/salvar-curso'     => Persistencia::class,
     '/excluir-curso'    => Exclusao::class,
     '/alterar-curso'    => FormularioEdicao::class,
+    '/login'            =>FormularioLogin::class,
+    '/realiza-login'    => RealizarLogin::class,
 ];
 
 return $rotas;
